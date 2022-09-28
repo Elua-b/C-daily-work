1c8bc0354d780d0ffa9b10beaae074512ed40b62
Elua-b <bugingoeloi@gmail.com>
2022-09-28 19:33:44 +0200

    continuous

---
+++ exc1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+ int main (){
+ 	string firstName;
+ 	string lastName;
+ 	int age;
+ 	 cout <<"Enter your first name:" ;
+ 	    cin>>firstName;
+ 	 cout <<"Enter your last name:" ;
+ 	 	cin>>lastName;
+ 	 cout <<"Enter your age:" ;
+ 	 	cin>>age;
+ 	 cout<<"Your names are" <<firstName <<lastName <<"with age of "<<age 	;
+ 	 
+	  return 0;
+	   	
+ 	 
+ }
---
+++ hello2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	string name;
+	int age;
+	cout <<"Enter your name";
+	cin>> name;
+	cout <<"Enter your age";
+	cin>>age;
+	cout << "My name is "<<name<<"I am "<<age<<"years old";
+	return 0;
+}
