b447b7892900c92f5739681a137bdf19275716e2
Elua-b <bugingoeloi@gmail.com>
2022-10-12 13:06:13 +0200

    the class on bitwise

---
+++ bitwise3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a=12;
+	int b=25;
+	cout << "a andb"<<(a<<1)<<endl;
+	cout << "a andb"<<(a<<2)<<endl;
+	cout << "a andb"<<(a<<3)<<endl;
+	cout << "a andb"<<(a<<4)<<endl;
+	
+	
+}
---
+++ exercise1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main (){
+	int marks;
+	cout <<"Enter your Marks: " ;
+ 	 cin>>marks;
+ 	
+	 marks>10 ? cout<<"you have passed an exam " :  cout<<"you haven't passed an exam";
+
+ 	
+ 	 
+}
